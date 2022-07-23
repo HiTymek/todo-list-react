fe2490eb6aa739a7ec379935f9a5b66ba8a91c15
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-07-23 17:59:37 +0200

    task list without interactions yet

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -2,21 +2,20 @@ import "./style.css";
 
 
 const Buttons = ({ tasks, hideDoneTasks }) => {
-    if (tasks.length === 0) {
-        return null;
-    }
-    return (
-        <div className="section__headerButtons">
-            <button className="section__headerButton">
-                {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
-            </button >
-            <button
-                className="section__headerButton"
-                disabled={tasks.every(({ done }) => done)}>
-                Ukończ wszystkie
-            </button>
-        </div>
-    );
+    <div className="section__headerButtons">
+        {tasks.length > 0 && (
+            <>
+                <button className="section__headerButton">
+                    {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+                </button >
+                <button
+                    className="section__headerButton"
+                    disabled={tasks.every(({ done }) => done)}>
+                    Ukończ wszystkie
+                </button>
+            </>
+        )}
+    </div>
 };
 
 
