814249a47530017bc0d6e5a307baccf57df14010
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-07-28 20:32:03 +0200

    repaired a small syntax error in Buttons element

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,21 +1,23 @@
 import "./style.css";
 
 const Buttons = ({ tasks, hideDoneTasks }) => {
-    <div className="section__headerButtons">
-        {tasks.length > 0 && (
-            <>
-                <button className="section__headerButton">
-                    {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
-                </button>
-                <button
-                    className="section__headerButton"
-                    disabled={tasks.every(({ done }) => done)}
-                >
-                    Ukończ wszystkie
-                </button>
-            </>
-        )}
-    </div>
+    return (
+        <div className="section__headerButtons">
+            {tasks.length > 0 && (
+                <>
+                    <button className="section__headerButton">
+                        {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+                    </button>
+                    <button
+                        className="section__headerButton"
+                        disabled={tasks.every(({ done }) => done)}
+                    >
+                        Ukończ wszystkie
+                    </button>
+                </>
+            )}
+        </div>
+    )
 };
 
 export default Buttons;
