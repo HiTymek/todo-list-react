c0d1b32ff5cacb67ec0809222fb07ef6a62547e2
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-09-04 14:20:02 +0200

    delete small bug with localStorage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { useState, useEffect } from 'react';
 
 
 function App() {
-  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || "[]");
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
 
   useEffect(() => {
     localStorage.setItem("tasks", JSON.stringify(tasks));
