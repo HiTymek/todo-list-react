25935683c7819b573a21ff4fef302c429d3ab3a6
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-09-30 21:55:41 +0200

    styled-components added to Header component

---
+++ src/Header/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+    margin: 40px auto 20px auto;
+`;
+
+export const Title = styled.h1`
+    margin-left: 20px;
+`;
