5d059b845072bc7b88e127dea47944479e814675
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-10-30 14:43:54 +0100

    add Navigation to details task window

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -5,23 +5,27 @@ import { ContentContainer } from "../../author/styled";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { getTaskById } from "../tasksSlice";
+import Navigation from "../../../common/Navigation";
 
 const TaskPage = () => {
     const { id } = useParams();
     const task = useSelector(state => getTaskById(state, id));
 
     return (
-        <Container>
-            <Header title="Szczegóły zadania" />
-            <Section
-                title={task ? task.content : "Nie znaleziono zadania 😥"}
-                body={
-                    <ContentContainer>
-                        <strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}
-                    </ContentContainer>
-                }
-            />
-        </Container>
+        <>
+            <Navigation />
+            <Container>
+                <Header title="Szczegóły zadania" />
+                <Section
+                    title={task ? task.content : "Nie znaleziono zadania 😥"}
+                    body={
+                        <ContentContainer>
+                            <strong>Ukończono:</strong> {task.done ? "Tak" : "Nie"}
+                        </ContentContainer>
+                    }
+                />
+            </Container>
+        </>
     );
 };
 
